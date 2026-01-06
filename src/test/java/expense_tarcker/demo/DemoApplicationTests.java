d0a3b180ceef89a8f883bd3418856eece1bc1e37
Pandi <pandik9585@gmail.com>
2026-01-06 11:48:39 +0530

    Initial commit - EMS Spring Boot Maven

---
+++ src/test/java/expense_tarcker/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package expense_tarcker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
